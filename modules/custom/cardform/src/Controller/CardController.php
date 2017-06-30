fb245d104a21f4a75b557de83fa945f35cb6b010
TGR05 <tgrosrenaud@gmail.com>
2017-06-30 17:29:11 +0200

    Commit de Vendredi

---
+++ modules/custom/cardform/src/Controller/CardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\cardform\Controller;
+ 
+use Drupal\Core\Controller\ControllerBase;
+ 
+class CardController extends ControllerBase {
+	
+  public function content() {
+  };
+}
---
+++ modules/custom/cardform/src/Plugin/Block/CardformBlock.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\cardform\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'cardform' block.
+ *
+ * @Block(
+ *   id = "cardform_block",
+ *   admin_label = @Translation("cardform"),
+ *   category = @Translation("cardform")
+ * )
+ */
+class CardformBlock extends BlockBase {
+
+ /*  protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'search content');
+  }
+
+  public function build() {
+    return \Drupal::formBuilder()->getForm('Drupal\search\Form\SearchBlockForm');
+  }
+ */
+ 
+/*    public function build() {
+    return array(
+      '#markup' => $this->t('Hello, World!'),
+    );
+	
+	return array(
+      '#theme' => 'node--business-cards--full',
+    );
+	
+	
+  } */
+  
+  public function build() {
+  return array(
+		'#title' => 'PREVIEW',
+		'#theme' => 'business_cards',
+			'#company' => 'company',
+			'#job' => 'job',
+			'#name' => 'name',
+			'#lastname' => 'lastname',
+			'#address' => 'field_adress',
+			'#num' => 'num',
+			'#email' => 'email',
+    );
+  }
+ 
+ 
+}
