a3e62133dfaf83ac768b8d4b498da9ae58157515
Nirina <nirina.andriamanantsara@gmail.com>
2017-07-06 13:22:58 +0200

    Added switching pattern functionnality

--- modules/custom/cardform/js/businesscards.js
+++ modules/custom/cardform/js/businesscards.js
@@ -10,30 +10,31 @@
   $("#edit-field-address-0-value").keypress(update);
   $("#edit-field-num-0-value").keypress(update);
   $("#edit-field-mail-0-value").keypress(update);
+  $("#edit-field-models").change(update);
   $("#edit-field-website-0-value").keypress(update);
 });
 
 function update(){
   $('.right').slideDown('slow');
-  //var modelpro = $("#edit-field-img-pro-card-0-upload").val();
   var company     = (($("#edit-title-0-value").val() != "") 			     ? $("#edit-title-0-value").val() 		        : "Company");
   var firstName   = (($("#edit-field-first-name-0-value").val() != "") ? $("#edit-field-first-name-0-value").val() 	: "First Name");
-  var lastName    = (($("#edit-field-last-name-0-value").val() != "") 		   ? $("#edit-field-last-name-0-value").val() 		    : "Last Name") ;
+  var lastName    = (($("#edit-field-last-name-0-value").val() != "")  ? $("#edit-field-last-name-0-value").val() 	: "Last Name") ;
   var job         = (($("#edit-field-job-0-value").val() != "") 		   ? $("#edit-field-job-0-value").val() 		    : "Job");
   var address     = (($("#edit-field-address-0-value").val() != "") 	 ? $("#edit-field-address-0-value").val() 		: "Address") ;
   var phoneNumber = (($("#edit-field-num-0-value").val() != "") 	     ? $("#edit-field-num-0-value").val()         : "Phone Number");
   var email       = (($("#edit-field-mail-0-value").val() != "") 			 ? $("#edit-field-mail-0-value").val() 			  : "Email Address");
-  var website     = (($("#edit-field-website-0-value").val() != "") 			                 ? $("#edit-field-website-0-value").val() 		                    : "Website") ;
+  var pattern     = $("#edit-field-models").val();
+  var website     = (($("#edit-field-website-0-value").val() != "")    ? $("#edit-field-website-0-value").val() 		: "Website") ;
 
 
   //When selecting the model, loading the CSS file.
 
-  /*$(function(){
+  $(function(){
     $(document.head.lastChild).remove();
     $("head").append(
-      $(document.createElement("link")).attr({rel:"stylesheet", type:"text/css", href:"css/" + modelpro + ".css"})
+      $(document.createElement("link")).attr({rel:"stylesheet", type:"text/css", href:"/cardsfactory/modules/custom/cardform/css/businesscards/" + pattern + ".css"})
     );
-  });*/
+  });
 
   $('#displayCompany').html(company);
   $('#displayFirstName').html(firstName);
