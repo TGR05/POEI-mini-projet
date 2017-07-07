5a5ff2d83de96e3a3780f39c7236e8ed464be484
Nirina <nirina.andriamanantsara@gmail.com>
2017-07-07 23:38:21 +0200

    Managed background and position switching

--- modules/custom/cardform/js/businesscards.js
+++ modules/custom/cardform/js/businesscards.js
@@ -3,55 +3,77 @@
 
   $('.right').hide();
   $("#edit-field-img-pro-card-0-upload").click(update);
-  $("#edit-title-0-value").keypress(update);
-  $("#edit-field-first-name-0-value").keypress(update);
-  $("#edit-field-last-name-0-value").keypress(update);
-  $("#edit-field-job-0-value").keypress(update);
-  $("#edit-field-address-0-value").keypress(update);
-  $("#edit-field-num-0-value").keypress(update);
-  $("#edit-field-mail-0-value").keypress(update);
+  $("#edit-title-0-value").change(update);
+  $("#edit-field-first-name-0-value").change(update);
+  $("#edit-field-last-name-0-value").change(update);
+  $("#edit-field-job-0-value").change(update);
+  $("#edit-field-address-0-value").change(update);
+  $("#edit-field-num-0-value").change(update);
+  $("#edit-field-mail-0-value").change(update);
   $("#edit-field-models").change(update);
-  $("#edit-field-website-0-value").keypress(update);
+  $("#edit-field-position").change(update);
+  $("#edit-field-website-0-value").change(update);
 
+  if ($("span.file--image > a")) {
+    var bgimage = $("span.file--image > a").attr('href');
+    console.log('checking if there is span.file ' + bgimage);
+    $('#businesscardlayout').css('background-image', 'url( ' + bgimage + ')');
+  }
 
+  $(document).ajaxComplete(function(event, xhr, settings) {
+      if ($('.field--name-field-img-pro-card .ajax-new-content').hasClass('processed')) {
+        $('.ajax-new-content').remove();
 
-      $(document).ajaxComplete(function(event, xhr, settings) {
+        $('#businesscardlayout').removeAttr('style');
+        console.log('No background anymore by removing image ');
+        $('#edit-field-models').removeAttr('disabled');
+        return;
+      }
+      $('.field--name-field-img-pro-card .ajax-new-content').addClass('processed');
+      var bgimage = $("span.file--image > a").attr('href');
 
-          if ($('.field--name-field-img-pro-card .ajax-new-content').hasClass('processed')) {
-              return;
-          }
-          console.log('test');
-          $('.field--name-field-img-pro-card .ajax-new-content').addClass('processed');
-
-          // get img href
-
-
-          // set href to src img in template preview
-
-      });
+      $('#businesscardlayout').css('background-image', 'url( ' + bgimage + ')');
+      $('#edit-field-models').val('none');
+      console.log('Deactivate Select background image');
+      $('#edit-field-models').attr('disabled', 'disabled');
+  });
 });
 
 function update(){
   $('.right').slideDown('slow');
-  var company     = (($("#edit-title-0-value").val() != "") 			     ? $("#edit-title-0-value").val() 		        : "Company");
-  var firstName   = (($("#edit-field-first-name-0-value").val() != "") ? $("#edit-field-first-name-0-value").val() 	: "First Name");
-  var lastName    = (($("#edit-field-last-name-0-value").val() != "")  ? $("#edit-field-last-name-0-value").val() 	: "Last Name") ;
-  var job         = (($("#edit-field-job-0-value").val() != "") 		   ? $("#edit-field-job-0-value").val() 		    : "Job");
-  var address     = (($("#edit-field-address-0-value").val() != "") 	 ? $("#edit-field-address-0-value").val() 		: "Address") ;
-  var phoneNumber = (($("#edit-field-num-0-value").val() != "") 	     ? $("#edit-field-num-0-value").val()         : "Phone Number");
-  var email       = (($("#edit-field-mail-0-value").val() != "") 			 ? $("#edit-field-mail-0-value").val() 			  : "Email Address");
-  var pattern     = $("#edit-field-models").val();
-  var website     = (($("#edit-field-website-0-value").val() != "")    ? $("#edit-field-website-0-value").val() 		: "Website") ;
-
-
-  //When selecting the model, loading the CSS file.
+  var company            = (($("#edit-title-0-value").val() != "") 			      ? $("#edit-title-0-value").val() 		        : "Company");
+  var firstName          = (($("#edit-field-first-name-0-value").val() != "") ? $("#edit-field-first-name-0-value").val() : "First Name");
+  var lastName           = (($("#edit-field-last-name-0-value").val() != "")  ? $("#edit-field-last-name-0-value").val() 	: "Last Name") ;
+  var job                = (($("#edit-field-job-0-value").val() != "") 		    ? $("#edit-field-job-0-value").val() 		    : "Job");
+  var address            = (($("#edit-field-address-0-value").val() != "") 	  ? $("#edit-field-address-0-value").val() 		: "Address") ;
+  var phoneNumber        = (($("#edit-field-num-0-value").val() != "") 	      ? $("#edit-field-num-0-value").val()        : "Phone Number");
+  var email              = (($("#edit-field-mail-0-value").val() != "") 			? $("#edit-field-mail-0-value").val() 			: "Email Address");
+  var patternBgImage     = $("#edit-field-models").val();
+  var posText            = $("#edit-field-position").val();
+  var website            = (($("#edit-field-website-0-value").val() != "")    ? $("#edit-field-website-0-value").val() 		: "Website") ;
+  console.log(patternBgImage);
+  console.log("/cardsfactory/modules/custom/cardform/images/cardmodels/businesscards/" + patternBgImage);
+  if (patternBgImage != "_none") {
+    if (patternBgImage != null) {
+      $('#businesscardlayout').css('background-image', 'url("/cardsfactory/modules/custom/cardform/images/cardmodels/businesscards/' + patternBgImage + '")');
+      console.log("1 " + patternBgImage);
+    }
+    else {
+      patternBgImage = $("span.file--image > a").attr('href');
+      console.log("2 " + patternBgImage);
+      $('#businesscardlayout').css('background-image', 'url( ' + patternBgImage + ')');
+    }
+  }
+  //When selecting the position, loading the CSS file with text positions.
 
   $(function(){
-    // alert('haaaaa' + pattern); //
     $(document.head.lastChild).remove();
-    $("head").append(
-      $(document.createElement("link")).attr({rel:"stylesheet", type:"text/css", href:"/modules/custom/cardform/css/businesscards/" + pattern + ".css"})
-    );
+    if (posText != "_none") {
+      $("head").append(
+        //Note: be carefull, Thomas version is "/modules/custom/cardform/css/businesscards/"
+        $(document.createElement("link")).attr({rel:"stylesheet", type:"text/css", href:"/cardsfactory/modules/custom/cardform/css/businesscards/" + posText + ".css"})
+      );
+    }
   });
 
   $('#displayCompany').html(company);
